a8da359481ab199286d414161e84dfe2ef39f7e7
bartoszduch <139812577+bartoszduch@users.noreply.github.com>
2024-05-11 00:18:07 +0200

    Get_data_implementation_backend

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from get_weather import get_weather_data, count_energy
+from datetime import datetime
+
+def get_float_input():
+    while True:
+        try:
+            value = float(input())
+            return value
+        except ValueError:
+            print("Enter float number.")
 
 print("Enter latitude")
-latitude = float(input())
+while True:
+    latitude = get_float_input()
+    if -90<=latitude<=90:
+        break
+    else:
+        print("Latitude must be between -90 and 90")
 print("Enter longitude")
-longitude = float(input())
-
-if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
-    print("Latitude must be between -90 and 90. Longitude must be between -180 and 180")
-    exit()
+while True:
+    longitude = get_float_input()
+    if -180<=longitude<=180:
+        break
+    else:
+        print("Longitute must be between -180 and 180")
 
 response = get_weather_data(latitude, longitude)
 
