15843d3ba66a352fa194997d3d65db1c29854c9f
bartoszduch <139812577+bartoszduch@users.noreply.github.com>
2024-05-10 21:30:47 +0200

    Get_data implementation

--- data_weather.py
+++
@@ -1,7 +0,0 @@
-from get_weather import get_weather_data
-
-# Wywołanie funkcji z odpowiednimi parametrami
-response = get_weather_data(latitude='50', longitude='20')
-
-# Wypisanie odpowiedzi w konsoli
-print(response)
--- get_weather.py
+++ get_weather.py
@@ -1,7 +1,10 @@
 import requests
 from datetime import datetime
 
-def get_weather_data(latitude, longitude):
+def get_weather_data(lati, longi):
+    if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+        raise ValueError("Invalid latitude or longitude values. Latitude must be between -90 and 90. Longitude must be between -180 and 180.")
+
     # Endpoint API z danymi pogodowymi
     endpoint = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunshine_duration"
     print(endpoint)
@@ -19,7 +22,7 @@ def get_weather_data(latitude, longitude):
                 'weather_code': data["daily"]["weather_code"][i],
                 'temperature_min': data["daily"]["temperature_2m_min"][i],
                 'temperature_max': data["daily"]["temperature_2m_max"][i],
-                'sunshine_duration': data["daily"]["sunshine_duration"][i],
+                'sunshine_duration': data["daily"]["sunshine_duration"][i],#I'm not sure if I chose the sunlight time parameter correctly
             })
 
         return weather_data
@@ -28,13 +31,28 @@ def get_weather_data(latitude, longitude):
         # Obsługa błędów zapytania do API
         return {'error': str(e)}
 
-# Wywołanie funkcji z odpowiednimi parametrami
-latitude = '30'
-longitude = '10'
-response = get_weather_data(latitude, longitude)
 
-# Wypisanie odpowiedzi
-for i in range(0,7):
+def count_energy(resp):
+    energy_data=[]
+    for i in range(len(response)):
+        energy=2.5*0.2*response[i].get('sunshine_duration')
+        energy_data.append(energy)
+    return energy_data
+
+print("Enter latitude")
+latitude=float(input())
+print("Enter longitude")
+longitude=float(input())
+if not (-90 <=latitude<=90) or not (-180 <= longitude <= 180):
+    print("Latitude must be between -90 and 90. Longitude must be between -180 and 180")
+
+response = get_weather_data(latitude, longitude)
+energy=count_energy(response)
+print(energy)
+for i in range(len(response)):
     print(response[i])
+
+for i in range(len(energy)):
+    print("Generated energy in day "+str(response[i].get('date')+":"+str(energy[i])))
 
 
